8007255e427bc7e6600efa2f2d376a993ec32b60
NutsLyam <natalyailyamakova@yandex.ru>
2016-10-25 00:57:57 +0300

    Update IDataLayer.cs

--- Model/IDataLayer.cs
+++ Model/IDataLayer.cs
@@ -8,10 +8,13 @@ namespace LocalGram.Model
 {
    public interface IDataLayer
     {
-        Users AddUser(Users user);
+         Users AddUser(Users user);
         Posts AddPost(Posts post);
         Users GetUser(Guid id);
         Posts GetPost(Guid postId);
-
+        Comments AddComment(Comments comment);
+        Comments GetComment(Guid id);
+        Hashtags AddHashtag(Hashtags hashtag);
+        Hashtags GetHashtag(Guid id);
     }
 }
