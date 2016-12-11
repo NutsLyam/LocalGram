6933ca9a88050344f86340ad75b399ea26da138d
NutsLyam <natalyailyamakova@yandex.ru>
2016-12-12 00:08:57 +0300

    Add files via upload

---
+++ WPF/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LocalGram.WPF
+{
+    class MainWindowViewModel: INotifyPropertyChanged
+    {
+        private readonly HttpClientWrapper _httpClient = new HttpClientWrapper("http://localhost:53999/");
+        private string _userName;
+        private string _userId;
+
+    }
+}
